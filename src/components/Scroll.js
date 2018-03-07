1e4713d8f006434ec8124281f466aa160de3fea4
pgakk <35960144+pgakk@users.noreply.github.com>
2018-03-07 10:32:27 -0500

    Updated scroll height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{border: '3px solid black', height: '500px', overflow: 'scroll'}}>
+		<div style={{border: '3px solid black', height: '800px', overflow: 'scroll'}}>
 			{props.children}
 		</div>
 		);
